83efa7ff48087f40fa674259585ed8a289f763df
chipembere <chitiyobrian@gmail.com>
2020-02-04 09:55:03 +0000

    Adjusted generator function so it does not go out of range.

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 2. Create .gitignore
 3. Adjust generator import in routes.py
 4. Fix "getIdea" bug in index.html, changed to idea.
-5. Changed 'POST' to 'GET' in routes.py
+5. Change 'POST' to 'GET' in routes.py
 6. Re-placed the capitalize() function in generator.py
+7. Add -1 to generator so the list does not go out of range.
--- app/generator.py
+++ app/generator.py
@@ -4,7 +4,7 @@ activities = ['shopping', 'air travel', 'work', 'taxi', 'walking', 'pets', 'chil
 people = ['mums', 'dads', 'children', 'brothers', 'sisters', 'families', 'married couples', 'teachers', 'unemployed people', 'wealthy people', 'politicians', 'aliens', 'farmers', 'city workers', 'teenagers', 'models', 'coach potatoes', 'pirates', 'waiters', 'air stewards', 'pilots', 'celebrities', 'average joes']
 
 def getRandomIdea():
-    i = random.randint(0, len(activities))
-    j = random.randint(0, len(people))
+    i = random.randint(0, len(activities)-1)
+    j = random.randint(0, len(people)-1)
 
     return activities[i].capitalize(), people[j].capitalize()
